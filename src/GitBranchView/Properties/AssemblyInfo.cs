4c593906f86484668e2be57fa083d250ad381153
Mattias Wijkström <mattiawi@axis.com>
2023-01-20 14:42:04 +0100

    Bumped version to 2.4

--- src/GitBranchView/Properties/AssemblyInfo.cs
+++ src/GitBranchView/Properties/AssemblyInfo.cs
@@ -1,34 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Git Branch View")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Git Branch View")]
-[assembly: AssemblyCopyright("Copyright © Mattias Wijkström 2021")]
+[assembly: AssemblyCopyright("Copyright © Mattias Wijkström 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("24381543-d19c-40f8-87ea-7c6395b84a56")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.*")]
+[assembly: AssemblyVersion("2.4.*")]
